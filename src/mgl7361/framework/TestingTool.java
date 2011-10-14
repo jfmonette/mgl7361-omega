0e78a5c5ef62c5842c9913c8f199b18ca8115691
JF Monette <jfmonette@gmail.com>
2011-10-14 16:47:44 -0400

    Added a ResultsFileWriter.

--- src/mgl7361/framework/TestingTool.java
+++ src/mgl7361/framework/TestingTool.java
@@ -1,10 +1,9 @@
 package mgl7361.framework;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import mgl7361.framework.results.ResultsFileWriter;
 import mgl7361.framework.results.TestCaseResult;
 
 public class TestingTool {
@@ -13,22 +12,14 @@ public class TestingTool {
 		TestSuite testSuite = new TestSuite();
 		TestCaseFactory factory = new TestCaseFactory();
 		List<TestCaseResult> testCaseResults = new ArrayList<TestCaseResult>();
+		ResultsFileWriter resultsFileWriter = new ResultsFileWriter();
 		
 		for (String className : getClassNamesFromArguments(args)) {
 			testSuite.add(factory.makeTestCase(className));
 			testCaseResults = testRunner.execute(testSuite);
 		}
 		
-		for (TestCaseResult testCaseResult : testCaseResults) {
-			try {
-				FileWriter fstream = new FileWriter("results.txt");
-				BufferedWriter out = new BufferedWriter(fstream);
-				out.write("Passed: " + testCaseResult.getNumberOfPassedTests() + " Failed: " + testCaseResult.getNumberOfFailedTests());
-				out.close();
-			} catch (Exception e) {
-				System.err.println("Error: " + e.getMessage());
-			}
-		}
+		resultsFileWriter.WriteResultsToFile(testCaseResults);
 	}
 	
 	private static List<String> getClassNamesFromArguments(String[] args) throws ClassNotFoundException {
